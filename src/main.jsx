2e6e9460f85741fcb650e2cbb5b7ef1d95841e50
Vireak-Both55 <limesak806@gmail.com>
2025-12-10 02:29:01 -0800

    book-stroe 6

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,27 @@ import ServicePage from './Pages/ServicePage.jsx'
 import { FilterCateProvider } from './Store/FilterCate.jsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode> 
     <Provider store={store}>
     <FilterCateProvider>
     <BrowserRouter>
     <Routes>
-      <Route path='/'  element = {<App />}>
-      <Route path='/home' element = {<HomePage/>}/>
-      <Route index element = {<HomePage/>}/>
-      <Route path='/deals' element = {<DealsPage/>} />
-      <Route path='/service'element = {<ServicePage/>} />
+      {/* CORRECTION: Removed 'index' from this parent route.
+        <App /> is now a layout component that uses <Outlet /> to render its children.
+      */}
+      <Route path='/' element = {<App />}>
+        
+        {/* Removed the duplicate <Route path='/home' ... /> */}
+        
+        {/* This is the primary index route. 
+          It renders <HomePage /> when the path is exactly '/' (inside the <App /> layout). 
+        */}
+        <Route index element = {<HomePage/>}/>
+        
+        <Route path='/deals' element = {<DealsPage/>} />
+        <Route path='/service' element = {<ServicePage/>} />
       </Route>
     </Routes>
     </BrowserRouter>
